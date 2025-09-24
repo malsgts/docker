71e572eeb93e91c0fbd5dd7dc34d706faa274364
manuel-sierra_1701 <malsgts@gmail.com>
2025-09-24 15:40:23 -0700

    Updated terraform files
    Updated terraform files

--- aws-terraform-infra/main.tf
+++ aws-terraform-infra/main.tf
@@ -126,7 +126,7 @@ resource "aws_db_instance" "rds_db" {
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
-  name                 = var.db_name
+# name                 = var.db_name
   username             = var.db_username
   password             = var.db_password
   parameter_group_name = "default.mysql8.0"
